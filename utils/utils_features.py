46e0fbef949f3ab66001969c76fb526d495f7360
SiyuanChen726 <114987284+SiyuanChen726@users.noreply.github.com>
2025-05-12 13:53:24 +0800

    Update utils_features.py

--- utils/utils_features.py
+++ utils/utils_features.py
@@ -26,8 +26,8 @@ from utils.vision_transformer import vit_base
 
 from transformers import AutoModel
 from transformers import AutoImageProcessor, ViTModel
-from huggingface_hub import login, hf_hub_download
-login()
+# from huggingface_hub import login, hf_hub_download
+# login()
 
 sys.path.append('../RandStainNA')
 from randstainna import RandStainNA
@@ -93,10 +93,10 @@ def get_model(model_name, device):
             normalize])
 
     elif model_name == "UNI":
-        local_dir = f"{custom_path}/UNI"
-        os.makedirs(local_dir, exist_ok=True) 
-        hf_hub_download("MahmoodLab/UNI", filename="pytorch_model.bin", local_dir=local_dir, force_download=True)
-        
+        # local_dir = f"{custom_path}/UNI"
+        # os.makedirs(local_dir, exist_ok=True) 
+        # hf_hub_download("MahmoodLab/UNI", filename="pytorch_model.bin", local_dir=local_dir, force_download=True)
+                
         model = timm.create_model(
             "vit_large_patch16_224", img_size=224, patch_size=16, init_values=1e-5, num_classes=0, dynamic_img_size=True
         )
@@ -111,9 +111,9 @@ def get_model(model_name, device):
         )
 
     elif model_name == "gigapath":
-        local_dir = f"{custom_path}/gigapath"
-        os.makedirs(local_dir, exist_ok=True)  
-        hf_hub_download("prov-gigapath/prov-gigapath", filename="pytorch_model.bin", local_dir=local_dir, force_download=True)
+        # local_dir = f"{custom_path}/gigapath"
+        # os.makedirs(local_dir, exist_ok=True)  
+        # hf_hub_download("prov-gigapath/prov-gigapath", filename="pytorch_model.bin", local_dir=local_dir, force_download=True)
         
         model = timm.create_model("hf_hub:prov-gigapath/prov-gigapath", pretrained=False)
         model.load_state_dict(torch.load(os.path.join(custom_path, 'gigapath', "pytorch_model.bin"), map_location="cpu"), strict=True)
