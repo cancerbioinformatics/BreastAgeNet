6cdc16990c3cc5251a5fa93842f0c58e4a796062
SiyuanChen726 <114987284+SiyuanChen726@users.noreply.github.com>
2025-05-12 10:17:12 +0800

    Update utils_features.py

--- utils/utils_features.py
+++ utils/utils_features.py
@@ -61,9 +61,10 @@ def eval_transforms(pretrained=False):
 
 
 
-
 def get_model(model_name, device):
-    custom_path ='./weights'
+    custom_path = './weights'
+    os.makedirs(custom_path, exist_ok=True)
+    
     if model_name == "resnet50":
         resnet50 = models.resnet50(pretrained=True)
         # Remove the final fully connected layer
@@ -91,11 +92,11 @@ def get_model(model_name, device):
             pth_transforms.ToTensor(),
             normalize])
 
-
     elif model_name == "UNI":
-        # local_dir = f"{custom_path}/UNI"
-        # os.makedirs(local_dir, exist_ok=True)  # create directory if it does not exist
-        # hf_hub_download("MahmoodLab/UNI", filename="pytorch_model.bin", local_dir=local_dir, force_download=True)
+        local_dir = f"{custom_path}/UNI"
+        os.makedirs(local_dir, exist_ok=True) 
+        hf_hub_download("MahmoodLab/UNI", filename="pytorch_model.bin", local_dir=local_dir, force_download=True)
+        
         model = timm.create_model(
             "vit_large_patch16_224", img_size=224, patch_size=16, init_values=1e-5, num_classes=0, dynamic_img_size=True
         )
@@ -110,9 +111,10 @@ def get_model(model_name, device):
         )
 
     elif model_name == "gigapath":
-        # local_dir = f"{custom_path}/gigapath"
-        # os.makedirs(local_dir, exist_ok=True)  # create directory if it does not exist
-        # hf_hub_download("prov-gigapath/prov-gigapath", filename="pytorch_model.bin", local_dir=local_dir, force_download=True)
+        local_dir = f"{custom_path}/gigapath"
+        os.makedirs(local_dir, exist_ok=True)  
+        hf_hub_download("prov-gigapath/prov-gigapath", filename="pytorch_model.bin", local_dir=local_dir, force_download=True)
+        
         model = timm.create_model("hf_hub:prov-gigapath/prov-gigapath", pretrained=False)
         model.load_state_dict(torch.load(os.path.join(custom_path, 'gigapath', "pytorch_model.bin"), map_location="cpu"), strict=True)
 
