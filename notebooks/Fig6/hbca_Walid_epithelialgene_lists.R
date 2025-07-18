e9b1a93854437e0b508641c2127b770bfcbdcf29
fsumayyamohamed <146728252+fsumayyamohamed@users.noreply.github.com>
2025-07-18 12:25:46 +0100

    Create hbca_Walid_epithelialgene_lists.R

---
+++ notebooks/Fig6/hbca_Walid_epithelialgene_lists.R
@@ -0,0 +1,3 @@
+lasp2_genes <- c("SLPI", "ALDH1A3", "KIT", "MMP7", "KRT8", "WFDC2", "LCN2", "MUC1", "TSPAN8", "CXCL17")
+lhs2_genes <- c("ESR1", "PGR", "FOXA1", "AREG", "GREB1", "TFF1", "BCL2", "CCND1", "MSMB", "IGFBP5")
+bmyo1_genes <- c("KRT5", "KRT14", "ACTA2", "TP63", "MYLK", "CNN1", "TAGLN", "TPM2", "VCL", "TIMP3")
